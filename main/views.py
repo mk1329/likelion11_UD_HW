6cabf59c785d51acec4a6799289bd4eb410287f9
mk1329 <mk1329@dgu.ac.kr>
2023-05-08 02:40:29 +0900

    feat:5주차과제2번_UD구현

--- main/views.py
+++ main/views.py
@@ -9,10 +9,11 @@ def create(request):
     new_blog.writer = request.POST['writer']
     new_blog.pub_date = timezone.now()
     new_blog.body = request.POST['body']
+    new_blog.image = request.FILES.get('image')
 
     new_blog.save()
 
-    return redirect('detail', new_blog.id)
+    return redirect('main:detail', new_blog.id)
 
 def mainpage(request):
     blogs = Blog.objects.all()
@@ -27,3 +28,21 @@ def new(request):
 def detail(request, id):
     blog = get_object_or_404(Blog, pk = id)
     return render(request, 'main/detail.html',{'blog':blog})
+
+def edit(request, id):
+    edit_blog = Blog.objects.get(id=id)
+    return render(request, 'main/edit.html', {'blog' : edit_blog})
+
+def update(request, id):
+    update_blog = Blog.objects.get(id=id)
+    update_blog.title = request.POST['title']
+    update_blog.writer = request.POST['writer']
+    update_blog.pub_date = timezone.now()
+    update_blog.body = request.POST['body']
+    update_blog.save()
+    return redirect('main:detail', update_blog.id)
+
+def delete(request, id):
+    delete_blog = Blog.objects.get(id=id)
+    delete_blog.delete()
+    return redirect('main:mainpage')
